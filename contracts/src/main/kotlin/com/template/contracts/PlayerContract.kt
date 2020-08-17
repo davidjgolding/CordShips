2a2cde4cacc47cfe5b893f6a92e04b4fe7c2aa81
phil-r3 <philip.oreilly@r3.com>
2020-08-17 17:31:05 +0100

    Adding player state

--- contracts/src/main/kotlin/com/template/contracts/PlayerContract.kt
+++ contracts/src/main/kotlin/com/template/contracts/PlayerContract.kt
@@ -1,7 +1,10 @@
 package com.template.contracts
 
+import com.template.states.PlayerState
 import net.corda.core.contracts.CommandData
 import net.corda.core.contracts.Contract
+import net.corda.core.contracts.requireSingleCommand
+import net.corda.core.contracts.requireThat
 import net.corda.core.transactions.LedgerTransaction
 
 // ************
@@ -9,17 +12,27 @@ import net.corda.core.transactions.LedgerTransaction
 // ************
 class PlayerContract : Contract {
     companion object {
-        // Used to identify our contract when building a transaction.
         const val ID = "com.template.contracts.PlayerContract"
     }
 
-    // A transaction is valid if the verify() function of the contract of all the transaction's input and output states
-    // does not throw an exception.
     override fun verify(tx: LedgerTransaction) {
-        // Verification logic goes here.
+
+        val command = tx.commands.requireSingleCommand<Commands>()
+
+        when(command.value) {
+
+            is Commands.AddBattleShips -> requireThat{
+
+                "There should be no input state." using (tx.inputs.isEmpty())
+                "There should be one output state." using (tx.outputs.size == 1)
+                "The output state should be of type BoardState." using (tx.outputs[0].data is PlayerState)
+
+                val outputPlayerState = tx.outputStates[0] as PlayerState
+                "The battle ship placement is not valid." using (GameContract.BoardUtils.checkIfValidBattleShipPlacement(outputPlayerState.battleShips))
+            }
+        }
     }
 
-    // Used to indicate the transaction's intent.
     interface Commands : CommandData {
         class AddBattleShips : Commands
     }
