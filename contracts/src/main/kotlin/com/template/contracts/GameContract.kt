ba70cebdc289c3df8450469cc1f9f7507a26e18e
phil-r3 <philip.oreilly@r3.com>
2020-08-17 18:08:40 +0100

    Updating player and game states

--- contracts/src/main/kotlin/com/template/contracts/GameContract.kt
+++ contracts/src/main/kotlin/com/template/contracts/GameContract.kt
@@ -1,9 +1,7 @@
 package com.template.contracts
 
 import com.template.states.GameState
-import net.corda.core.contracts.CommandData
-import net.corda.core.contracts.Contract
-import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.contracts.*
 import net.corda.core.transactions.LedgerTransaction
 
 // ************
@@ -12,11 +10,25 @@ import net.corda.core.transactions.LedgerTransaction
 class GameContract : Contract {
 
     companion object {
-        const val ID = "com.template.contracts.TemplateContract"
+        const val ID = "com.template.contracts.GameContract"
     }
 
     override fun verify(tx: LedgerTransaction) {
-        // Verification logic goes here.
+
+        val command = tx.commands.requireSingleCommand<Commands>()
+
+        when(command.value) {
+
+            is Commands.StartGame -> requireThat {
+
+            }
+            is Commands.SubmitTurn -> requireThat {
+
+            }
+            is Commands.EndGame -> requireThat {
+
+            }
+        }
     }
 
     interface Commands : CommandData {
@@ -28,26 +40,18 @@ class GameContract : Contract {
     class BoardUtils {
         companion object {
 
-            fun checkIfValidBattleShipPlacement(positions: List<Pair<Int, Int>>) : Boolean {
+            fun checkIfValidPositions(positions: List<Pair<Int, Int>>) : Boolean {
                 for(position in positions) {
-                    if (!checkIfValidBattleShipPlacement(position))
+                    if (!checkIfValidPosition(position))
                         return false
                 }
                 return true
             }
 
-            private fun checkIfValidBattleShipPlacement(position: Pair<Int, Int>) : Boolean {
+            private fun checkIfValidPosition(position: Pair<Int, Int>) : Boolean {
                 if(position.first < 0 || position.first > 10) return false;
                 if(position.second < 0 || position.second > 10) return false;
                 return true;
-            }
-
-            fun checkIfValidStartBoard(board: Array<CharArray>): Boolean {
-                return true
-            }
-
-            fun checkIfValidBoardUpdate(inputBoard: Array<CharArray>, outputBoard: Array<CharArray>, playerChar: Char): Boolean {
-                return true
             }
 
             fun isGameOver(gameState: GameState): Boolean {
@@ -61,6 +65,17 @@ class GameContract : Contract {
     }
 
     class Board {
-        val board: Array<CharArray> = Array(10, {charArrayOf('E', 'E', 'E', 'E', 'E', 'E','E', 'E', 'E', 'E')} )
+        val board: List<List<Int>> = listOf(
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0),
+                listOf(0,0,0,0,0,0,0,0,0,0)
+        )
     }
 }
