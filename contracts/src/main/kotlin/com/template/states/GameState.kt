5b15ac0acdfc7d3c8ca1401fe5e249ef8b3bbc5f
phil-r3 <philip.oreilly@r3.com>
2020-08-17 15:58:03 +0100

    Adding some stubs for game contract and game state

---
+++ contracts/src/main/kotlin/com/template/states/GameState.kt
@@ -0,0 +1,49 @@
+package com.template.states
+
+import com.template.contracts.GameContract
+import net.corda.core.contracts.*
+import net.corda.core.identity.AbstractParty
+import net.corda.core.identity.AnonymousParty
+import net.corda.core.identity.Party
+import net.corda.core.serialization.CordaSerializable
+
+
+// *********
+// * State *
+// *********
+@CordaSerializable
+enum class GameStatus {
+    GAME_IN_PROGRESS, GAME_OVER
+}
+
+@BelongsToContract(GameContract::class)
+@CordaSerializable
+data class GameState(val players: List<AbstractParty>,
+                     val boards: List<GameContract.Board>,
+                     val currentPlayer: Party,
+                     val status: GameStatus = GameStatus.GAME_IN_PROGRESS,
+                     override val linearId: UniqueIdentifier = UniqueIdentifier()): LinearState {
+
+    override val participants: List<AbstractParty> = players
+
+    private fun Array<CharArray>.copy() = Array(size) { get(it).clone() }
+
+    // Returns the party of the current player
+    fun getCurrentPlayerParty(): Party { return currentPlayer }
+
+    // Returns a copy of a BoardState object after a move at Pair<x,y>
+    fun returnNewBoardAfterMove(pos: Pair<Int,Int>, me:AnonymousParty, competitor:AnonymousParty): GameState {
+
+        // Check if the index is valid
+        if (false) throw IllegalStateException("Invalid board index.")
+
+        // Update who's turn it is
+        // Create a new game state
+        val newGameState = this
+
+        if (GameContract.BoardUtils.isGameOver(newGameState))
+            return newGameState.copy(status = GameStatus.GAME_OVER)
+
+        return newGameState
+    }
+}
